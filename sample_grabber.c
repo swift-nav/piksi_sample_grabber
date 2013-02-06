a873a69910c23285a77ee5928d5694c6b298f9a9
Colin Beighley <colinbeighley@gmail.com>
2013-02-05 18:28:15 -0800

    Re-added verbose and size command line arguments in sample_grabber

--- sample_grabber.c
+++ sample_grabber.c
@@ -20,11 +20,20 @@
  *             given file one sample per byte as signed integers.
  *
  *   Usage :   After running set_fifo_mode to set the FT232H on the Piksi to 
- *             sample streaming mode, use sample_grabber via
- *             ./sample_grabber [filename]
- *             End sample capture with ^C (CTRL+C).
- *             After finishing the sample capture, run set_uart_mode to set
- *             the FT232H on the Piksi back to UART mode for normal operation.
+ *             fifo mode, use sample_grabber (see arguments below).
+ *             End sample capture with ^C (CTRL+C). After finishing sample
+ *             capture, run set_uart_mode to set the FT232H on the Piksi back
+ *             to UART mode for normal operation.
+ *
+ *   Options : ./sample_grabber [-s number] [-v] [-h] [filename]
+ *             [--size -s]     Number of samples to collect before exiting.
+ *                             May be suffixed with a k (1e3) or an M (1e6).
+ *                             If no argument is supplied, samples will be
+ *                             collected until ^C (CTRL+C) is received.
+ *             [--verbose -v]  Print more verbose output.
+ *             [--help -h]     Print usage information and exit.
+ *             [filename]      A filename to save samples to. If none is 
+ *                             supplied then samples will not be saved.
  *
  *   Based on the example "stream_test.c" in libftdi, updated in 2010 by Uwe 
  *   Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de> for libftdi and 
@@ -34,6 +43,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <getopt.h>
 #include <signal.h>
 #include <errno.h>
@@ -42,22 +52,26 @@
 #include "ftdi.h"
 #include "pipe/pipe.h"
 
+/* Number of bytes to initially read out of device without saving to file */
 #define NUM_FLUSH_BYTES 50000
+/* Number of samples in each byte received (regardless of how they're packed)*/
 #define SAMPLES_PER_BYTE 2
 /* Number of bytes to read out of FIFO and write to disk at a time */
 #define WRITE_SLICE_SIZE 50 
-#define PIPE_SIZE 0 /* 0 means size is unconstrained */
+/* Maximum number of elements in pipe - 0 means size is unconstrained */
+#define PIPE_SIZE 0
 
 /* FPGA FIFO Error Flag is 0th bit, active low */
 #define FPGA_FIFO_ERROR_CHECK(byte) (!(byte & 0x01))
 
-static uint64_t total_bytes_saved = 0;
+static uint64_t total_unflushed_bytes = 0;
+static long int bytes_wanted = 0; /* 0 means uninitialized */
 
 /* Samples from the device are {sign,msb mag,lsb mag}. Can index mapping */
 /* with each received signmag sample to convert to signed (signed int 8) */
 static const char mapping[8] = {1, 3, 5, 7, -1, -3, -5, -7};
 
-static FILE *outputFile;
+static FILE *outputFile = NULL;
 
 static int exitRequested = 0;
 
@@ -67,24 +81,26 @@ static pthread_t file_writing_thread;
 static pipe_producer_t* pipe_writer;
 static pipe_consumer_t* pipe_reader;
 
-/*
- * sigintHandler --
- *
- *    SIGINT handler, so we can gracefully exit when the user hits ctrl-C.
- */
-static void
-sigintHandler(int signum)
+static int verbose = 0;
+
+static void sigintHandler(int signum)
 {
   exitRequested = 1;
 }
 
-static void
-usage(void)
+static void print_usage(void)
 {
   printf(
-  "Usage: ./sample_grabber [filename] \n"
-  "       If some filename is given, write data read to that file. Progess\n"
-  "       information is printed each second. End sample capture with ^C.\n"
+  "Usage: ./sample_grabber [-s num] [-v] [-h] [filename]\n"
+  "Options:\n"
+  "  [--size -s]     Number of samples to collect before exiting. Number may\n"
+  "                  be suffixed with a k (1e3) or an M (1e6). If no argument\n"
+  "                  is supplied, samples will be collected until ^C (CTRL+C)\n"
+  "                  is received.\n"
+  "  [--verbose -v]  Print more verbose output.\n"
+  "  [--help -h]     Print usage information and exit.\n"
+  "  [filename]      A filename to save samples to. If none is supplied then\n"
+  "                  samples will not be saved.\n"
   "Note : set_fifo_mode must be run before sample_grabber to configure the USB\n"
   "       hardware on the device for FIFO mode. Run set_uart_mode after\n"
   "       sample_grabber to set the device back to UART mode for normal\n"
@@ -93,9 +109,57 @@ usage(void)
   exit(1);
 }
 
-static uint32_t n_err = 0;
-static int
-readCallback(uint8_t *buffer, int length, FTDIProgressInfo *progress, void *userdata)
+/** Parse a string representing a number of samples to an integer.  String can
+ * be a plain number or can include a unit suffix. This can be one of 'k' or
+ * 'M' which multiply by 1e3 and 1e6 respectively.
+ *
+ * e.g. "5" -> 5
+ *      "2k" -> 2000
+ *      "3M" -> 3000000
+ *
+ * Returns -1 on an error condition.
+ */
+long int parse_size(char * s)
+{
+  long int val = -1;
+  char last = s[strlen(s)-1];
+
+  /* If the last character is a digit then just return the string as a
+   * number.
+   */
+  if (isdigit(last)) {
+    val = atol(s);
+    if (val != 0)
+      return val;
+    else
+      return -1;
+  }
+
+  /* Last char is a unit suffix, find the numeric part of the value. */
+  /* Delete the suffix. */
+  s[strlen(s)-1] = 0;
+  /* Convert to a long int. */
+  val = atol(s);
+  if (val == 0)
+    return -1;
+
+  /* Multiply according to suffix and return value. */
+  switch (last) {
+    case 'k':
+    case 'K':
+      return val*1e3;
+      break;
+
+    case 'M':
+      return val*1e6;
+      break;
+
+    default:
+      return -1;
+  }
+}
+
+static int readCallback(uint8_t *buffer, int length, FTDIProgressInfo *progress, void *userdata)
 {
   /*
    * Keep track of number of bytes read - don't record samples until we have 
@@ -103,6 +167,7 @@ readCallback(uint8_t *buffer, int length, FTDIProgressInfo *progress, void *user
    * - it is necessary to do this to ensure we receive continuous samples.
    */
   static uint64_t total_num_bytes_received = 0;
+
   /* Array for extraction of samples from buffer */
   char *conv_buf = (char *)malloc(length*SAMPLES_PER_BYTE*sizeof(char));
   if (length){
@@ -124,9 +189,8 @@ readCallback(uint8_t *buffer, int length, FTDIProgressInfo *progress, void *user
         for (uint64_t ci = 0; ci < length; ci++){
           /* Check byte to see if a FIFO error occured */
           if (FPGA_FIFO_ERROR_CHECK(buffer[ci])) {
-            perror("FPGA FIFO Error Flag");
-            printf("num samples taken = %ld\n",
-                   (long int)(total_bytes_saved+ci));
+            printf("FPGA FIFO Error Flag at sample number %ld\n",
+                   (long int)(total_unflushed_bytes+ci));
             exitRequested = 1;
           }
           /* Extract samples from buffer, convert, and store in conv_buf */
@@ -137,19 +201,28 @@ readCallback(uint8_t *buffer, int length, FTDIProgressInfo *progress, void *user
         }
         /* Push values into the pipe */
         pipe_push(pipe_writer,(void *)conv_buf,length*2);
-        total_bytes_saved += length;
       }
+      total_unflushed_bytes += length;
     }
     total_num_bytes_received += length;
   }
-  if (progress){
-    fprintf(stderr, "%10.02fs total time %9.3f MiB captured %7.1f kB/s curr rate %7.1f kB/s totalrate %d dropouts\n",
-            progress->totalTime,
-            progress->current.totalBytes / (1024.0 * 1024.0),
-            progress->currentRate / 1024.0,
-            progress->totalRate / 1024.0,
-            n_err);
+
+  /* bytes_wanted = 0 means program was not run with a size argument */
+  if (bytes_wanted != 0 && total_unflushed_bytes >= bytes_wanted){
+    exitRequested = 1;
   }
+
+  /* Print progress : time elapsed, bytes transferred, transfer rate */
+  if (verbose) {
+    if (progress){
+      printf("%10.02fs total time %9.3f MiB captured %7.1f kB/s curr %7.1f kB/s total\n",
+              progress->totalTime,
+              progress->current.totalBytes / (1024.0 * 1024.0),
+              progress->currentRate / 1024.0,
+              progress->totalRate / 1024.0);
+    }
+  }
+
   return exitRequested ? 1 : 0;
 }
 
@@ -170,33 +243,65 @@ static void* file_writer(void* pc_ptr){
 }
 
 int main(int argc, char **argv){
-   struct ftdi_context *ftdi;
-   int err, c;
-   FILE *of = NULL;
-   char const *outfile  = 0;
-   outputFile =0;
-   exitRequested = 0;
-   char *descstring = NULL;
-   int option_index;
-   static struct option long_options[] = {{NULL},};
+  struct ftdi_context *ftdi;
+  int err;
+  FILE *of = NULL;
+  char const *outfile  = 0;
+  outputFile =0;
+  exitRequested = 0;
+  char *descstring = NULL;
 
-   while ((c = getopt_long(argc, argv, "h", long_options, &option_index)) !=- 1)
-     switch (c) 
-     {
-     case 'h':
-       usage();
-       break;
-     default:
-       usage();
+  static const struct option long_opts[] = {
+    {"size",       required_argument, NULL, 's'},
+    {"verbose",    no_argument,       NULL, 'v'},
+    {"help",       no_argument,       NULL, 'h'},
+    {NULL,         no_argument,       NULL, 0}
+  };
+
+  opterr = 0;
+  int c;
+  int option_index = 0;
+  while ((c = getopt_long(argc, argv, "s:vh", long_opts, &option_index)) != -1)
+    switch (c) {
+      case 'v':
+        verbose++;
+        break;
+      case 's': {
+        long int samples_wanted = parse_size(optarg);
+        if (samples_wanted <= 0) {
+          fprintf(stderr, "Invalid size argument.\n");
+          return EXIT_FAILURE;
+        }
+        bytes_wanted = samples_wanted / SAMPLES_PER_BYTE;
+        if (bytes_wanted <= 0) {
+          fprintf(stderr, "Invalid number of bytes to transfer.\n");
+          return EXIT_FAILURE;
+        }
+        break;
+      }
+      case 'h':
+        print_usage();
+        return EXIT_SUCCESS;
+      case '?':
+        if (optopt == 's')
+          fprintf(stderr, "Transfer size option requires an argument.\n");
+        else
+          fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+        return EXIT_FAILURE;
+      default:
+        abort();
      }
    
-   if (optind == argc - 1){
+   if (optind < argc - 1){
+     // Too many extra args
+     print_usage();
+   } else if (optind == argc - 1){
      // Exactly one extra argument- a dump file
      outfile = argv[optind];
-   }
-   else if (optind < argc){
-     // Too many extra args
-     usage();
+   } else {
+     if (verbose) {
+       printf("No file name given, will not save samples to file\n");
+     }
    }
    
    if ((ftdi = ftdi_new()) == 0){
@@ -266,7 +371,9 @@ int main(int argc, char **argv){
      fclose(outputFile);
      outputFile = NULL;
    }
-   fprintf(stderr, "Capture ended.\n");
+   if (verbose) {
+     printf("Capture ended.\n");
+   }
    
    /* Clean up */
    if (ftdi_set_bitmode(ftdi, 0xff, BITMODE_RESET) < 0){
